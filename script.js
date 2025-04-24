6760d5dc25815ea29e1ce89eaa2b1ec161e7fd4c
emiliano-chp-dev <echp.dev@gmail.com>
2025-04-24 17:58:23 +0300

    Create function that returns computer player choice

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello World!")
+function getComputerChoice() {
+    let computerChoice = '';
+    let randomInt = getRandomInt(3);
+
+    if (randomInt === 0) {
+        computerChoice = 'rock';
+    } else if (randomInt === 1) {
+        computerChoice = 'paper';
+    } else {
+        computerChoice = 'scissors';
+    }
+
+    return computerChoice;
+}
+
+function getRandomInt (num) {
+    return Math.floor(Math.random() * num);
+}
+
+console.log(getComputerChoice());
