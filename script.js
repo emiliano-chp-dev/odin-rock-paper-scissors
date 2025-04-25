7f2f48eea8e2533d08fab76d339a92ca0954b41b
emiliano-chp-dev <echp.dev@gmail.com>
2025-04-25 18:21:41 +0300

    Write function to play one round and log return value to console

--- script.js
+++ script.js
@@ -17,13 +17,40 @@ function getRandomInt (num) {
     return Math.floor(Math.random() * num);
 }
 
-console.log(getComputerChoice());
-
 function getHumanChoice() {
-    return prompt("Rock, paper, or scissors?").toLocaleLowerCase();
+    return prompt("Rock, paper, or scissors?");
 }
-
-console.log(getHumanChoice());
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice.toLowerCase();
+    let roundResult = "";
+
+    if (humanChoice === computerChoice) {
+        roundResult = "It's a draw!";
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        roundResult = "You lose! Paper beats rock.";
+        computerScore++;
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        roundResult = "You win! Rock beats scissors.";
+        computerScore++;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        roundResult = "You win! Paper beats rock.";
+        humanScore++;
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        roundResult = "You lose! Scissors beats paper.";
+        computerScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        roundResult = "You lose! Rock beats scissors.";
+        computerScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        roundResult = "You win! Scissors beats paper."
+        humanScore++;
+    }
+
+    return roundResult;
+}
+
+console.log(playRound(getHumanChoice(), getComputerChoice()));
